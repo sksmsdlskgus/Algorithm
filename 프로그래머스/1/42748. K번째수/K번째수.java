c92839846862e141869f46f5597c264539ca8fe4
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2024-12-28 19:59:24 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] result = new int[commands.length];
+
+        // commands 배열을 순회
+        for (int idx = 0; idx < commands.length; idx++) {
+            int i = commands[idx][0];
+            int j = commands[idx][1]; 
+            int k = commands[idx][2];
+
+            // 1. 배열 자르기
+            int[] subArray = Arrays.copyOfRange(array, i - 1, j);
+
+            Arrays.sort(subArray);
+
+            result[idx] = subArray[k - 1];
+        }
+
+        return result;
+    }
+}
