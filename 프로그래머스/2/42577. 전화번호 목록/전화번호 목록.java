581a8dbf80b5a23c436ee3d00be079f6a6209a23
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-01-05 17:54:24 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,22 +3,13 @@ class Solution {
     public boolean solution(String[] phone_book) {
         // phone_book = 전화번호를 담은 배열 
         // 한 번호가 다른 번호의 접두어인 경우가 존재 = false 아니면 true
+        //String.startsWith() 활용해서 다시.
         Arrays.sort(phone_book);
-        
-        Map<String,Boolean> map = new HashMap<>();
-        for(String phoneNumber : phone_book ){
-            map.put(phoneNumber,true);
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i]))
+               return false;
         }
         
-        for(String phoneNumber : phone_book){
-            String include = "";
-            for(int i=0; i < phoneNumber.length()-1; i++){ // 자기자신 방지
-                include += phoneNumber.charAt(i);
-                    if(map.containsKey(include)){
-                     return false;
-              }
-            }       
-      }
         return true;
     }
 }
