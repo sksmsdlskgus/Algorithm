a7c60f96571356cb3cb51d4cdc5b6e9529689a65
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-04-26 19:13:38 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+    answer = 0
+    coupon = chicken
+
+    while coupon >= 10:
+        service = coupon // 10
+        answer += service
+        coupon = coupon % 10 + service
+
+    return answer
