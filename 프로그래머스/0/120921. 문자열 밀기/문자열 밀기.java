f662390c3f2d04d720f6a8486ecf2e1c695e7ebc
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-05-02 02:14:12 +0900

    [level 0] Title: 문자열 밀기, Time: 12.18 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String A, String B) {
+        
+        if (A.equals(B)) {
+            return 0;
+        }
+
+        for (int i = 0; i < A.length(); i++) {
+
+            A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);         
+            if (A.equals(B)) {
+                return i + 1; 
+            }
+        }
+
+        return -1;
+    }
+}
