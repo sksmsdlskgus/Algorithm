297417c0e5211b5d3bdf61217013bcb7deb3676d
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-05-07 18:44:53 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.08 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        // n = 사람 수 , k = 방법들 중 k번째 배열을 return
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++){ // n까지 다 넣기
+            numbers.add(i);
+        }
+        
+        long fact =1;
+        for(long i=1; i<n; i++){
+            fact*= i;
+        }
+        
+        k--; 
+        
+        for(int i=0; i<n; i++){
+            int index = (int)(k/fact); // 첫번째 숫자가 어떤 숫자가 올지
+            answer[i] = numbers.get(index);
+            numbers.remove(index);
+            
+            if(n-1-i >0){ // 그다음 숫자를 위해 경우의 수 줄이기
+                k %= fact;
+                fact /= (n-1-i);
+            }
+            
+        }
+        
+        return answer;
+    }
+}
