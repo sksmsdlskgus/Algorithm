9a989eedde94fc39fc9bde44c625c15241d377a3
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-05-10 17:12:27 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,17 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        // 컴퓨터 개수 = n
-        // 연결 정보 배열 = computers
-        // computers[i][j] 연결되면 == 1
-        // return 네트워크 개수  
         boolean[] visited = new boolean[n];
         int count = 0;
         
         for(int i=0; i<n; i++){
-            if(!visited[i]){ // 방문하지 않았던 노드라면?
-                bfs(i, computers, visited);
-                count++; // 새로운 네트워크
+            if(!visited[i]){
+                dfs(i, computers, visited);
+                count++; 
             }
         }
         
@@ -20,20 +16,12 @@ class Solution {
         return count;
     }
     
-    public void bfs (int start, int[][] computers, boolean[] visited){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+    public void dfs (int current, int[][] computers, boolean[] visited){
+        visited[current] = true; 
         
-        visited[start] = true; // 방문 처리
-        
-        while(!queue.isEmpty()){
-            int currentNode = queue.poll();
-            
-            for (int i = 0; i < computers.length; i++) {
-                if(computers[currentNode][i] == 1 && !visited[i]){
-                    queue.offer(i);
-                    visited[i] = true;
-                }
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[current][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited); 
             }
         }
         
