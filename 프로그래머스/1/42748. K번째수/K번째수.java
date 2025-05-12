e76a521a232a3ea3c086c8f9f02d2bd66489ccc0
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-05-12 18:50:36 +0900

    [level 1] Title: K번째수, Time: 3.90 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,26 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] result = new int[commands.length];
-
-        // commands 배열을 순회
-        for (int idx = 0; idx < commands.length; idx++) {
-            int i = commands[idx][0];
-            int j = commands[idx][1]; 
-            int k = commands[idx][2];
-
-            System.out.println(result);
+        
+        List<Integer> kList = new ArrayList<>(); // k번째 수 담기
+        
+        for(int i=0; i<commands.length; i++){
+            int start = commands[i][0]-1; //i
+            int end = commands[i][1]; //j
+            int k = commands[i][2]; //k
             
-            // 1. 배열 자르기
-            int[] subArray = Arrays.copyOfRange(array, i - 1, j);
-
+            int[] slice = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(slice);
             
-            Arrays.sort(subArray);
-
-            result[idx] = subArray[k - 1];
+            for(int j=0; j<slice.length; j++){
+                if(j==k-1){
+                    kList.add(slice[j]);
+                }
+            }
+            
         }
-
-        return result;
+        
+        return kList.stream().mapToInt(i->i).toArray();
     }
 }
