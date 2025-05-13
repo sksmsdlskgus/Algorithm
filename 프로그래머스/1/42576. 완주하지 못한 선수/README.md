b2e4188d11ee8a68161fe4a0b333f07ff6fcdba3
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-05-13 16:22:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 121.99 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33.1 MB, 시간: 39.35 ms
+메모리: 99.8 MB, 시간: 121.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 16:41:31
+2025년 05월 13일 16:22:39
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,28 +1,28 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 참가자 명단을 저장할 HashMap
-        HashMap<String, Integer> map = new HashMap<>();
+        // 완주하지 못한 선수의 이름을 return
+        // 동명이인 존재
+        String answer = "";
+        Map<String,Integer> map = new HashMap<>();
 
-        // 참가자 명단을 HashMap에 추가
-        for (String person : participant) {
-            map.put(person, map.getOrDefault(person, 0) + 1);
+        for(int i=0; i<participant.length; i++){
+            map.put(participant[i],map.getOrDefault(participant[i],0)+1);
         }
-
-        // 완주자 명단을 HashMap에서 차감
-        for (String person : completion) {
-            map.put(person, map.get(person) - 1);
-        }
-
-        // 값이 0이 아닌 선수를 반환
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                return key;
+        
+        for(int i=0; i<completion.length; i++){
+            map.put(completion[i],map.get(completion[i])-1);
+            
+            if(map.get(completion[i]) == 0){
+                map.remove(completion[i]);
             }
         }
-
-        // 모든 참가자가 완주한 경우 (문제 조건상 발생하지 않음)
-        return "";
+        
+        for(String name: map.keySet()){
+            answer = name;
+        }
+   
+        return answer;
     }
 }
