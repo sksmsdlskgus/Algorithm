35e72c680d652ff484fa3d035d9a1984c2cf75b3
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-06-05 23:29:50 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        // 2022년  -> age 나이이다.
+        // 쌤은 몇년도 사람일까 ?
+        
+        int answer = 0;
+        answer = 2022 - age + 1;
+        return answer;
+    }
+}
