da78f002fc9da283cb3a200c58db766a2913550a
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-06-18 17:34:04 +0900

    [level 1] Title: 과일 장수, Time: 81.53 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,12 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int solution(int k, int m, int[] score) {
         Arrays.sort(score); 
-        
         int n = score.length;
         int profit = 0;
 
         for (int i = n - m; i >= 0; i -= m) {
-            int min = score[i]; 
+            int min = score[i];
             profit += min * m;
         }
 
