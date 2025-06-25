2d02cef467514aebb1a06d57649a821356aaa165
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-06-25 16:54:24 +0900

    [level 2] Title: 더 맵게, Time: 1438.69 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        // k 가 될때 까지 = 안매운 스코빌 + (두번째로 안매운 스코빌*2)
-        // 최소 횟수 return 
-        // K이상으로 만들 수 없는 경우 -1
+        // 스코빌 지수 k이상으로 만들기 
+        // 가장 낮은 두개의 음식 
+        // 가장 안매운음식 + (두번째로 *2)
+        // 모두가 k 이상이 될때까지 반복
+        // 최소 횟수를 return 해라 -> 없을 경우에는 -1
         int count = 0;
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i: scoville){
+        for(int i:scoville){
             pq.offer(i);
         }
         
-        while (pq.size() > 1 && pq.peek() < K){
+        while(pq.size() > 1 && pq.peek() < K){
             int one = pq.poll();
-            int two = pq.poll();             
-            int mixed = one + (two * 2);
-            pq.offer(mixed);
+            int two = pq.poll();
             
-            count++;
+            int mixed = one +(two*2);
+            pq.offer(mixed);
+            count++;    
         }
-
+        
         return pq.peek() >= K ? count : -1;
     }
 }
