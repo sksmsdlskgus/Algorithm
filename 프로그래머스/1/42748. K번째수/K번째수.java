44fd669ae75f4608ede27b783d267e83e35f4635
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-10-10 15:48:56 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
-        List<Integer> kList = new ArrayList<>(); // k번째 수 담기
+        int[] answer = new int[commands.length];
         
         for(int i=0; i<commands.length; i++){
-            int start = commands[i][0]-1; //i
-            int end = commands[i][1]; //j
-            int k = commands[i][2]; //k
+            int frist = commands[i][0];
+            int second = commands[i][1];
+            int k = commands[i][2];
             
-            int[] slice = Arrays.copyOfRange(array, start, end);
+            int[] slice = Arrays.copyOfRange(array,frist-1,second);
             Arrays.sort(slice);
-            
-            for(int j=0; j<slice.length; j++){
-                if(j==k-1){
-                    kList.add(slice[j]);
-                }
-            }
-            
+            answer[i] = slice[k-1];
+
         }
         
-        return kList.stream().mapToInt(i->i).toArray();
+        return answer;
     }
 }
