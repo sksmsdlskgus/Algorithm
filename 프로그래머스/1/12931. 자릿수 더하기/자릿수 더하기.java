35b58a9988f5e8a6a22f4bb78e48b568c5d0139d
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-12-22 16:52:04 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.22 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,12 +3,14 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int sum = 0;
+
+        String n_string = Integer.toString(n);
         
-        String s = Integer.toString(n);
-        String[] sSplit = s.split("");
-        for(int i=0; i<sSplit.length; i++){
-            int sInt = Integer.parseInt(sSplit[i]);
-            sum += sInt;
+        System.out.println(n_string);
+        
+        for(char n_char: n_string.toCharArray()){
+            int n_int = Character.getNumericValue(n_char);
+            sum += n_int;
         }
         
         return sum;
