34d315b3fd28712279ae200de88cfa985a51e9e7
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-12-23 14:46:21 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.11 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,30 +1,28 @@
-import java.util.*;
 class Solution {
     public String solution(String s) {
-        // 짝수번째 알파벳은 대문자 
-        // 홀수번째 앒파벳은 소문자로 리턴 
-        
+        // 짝수번째는 대문자 
+        // 홀수번째는 소문자
+
         StringBuilder sb = new StringBuilder();
-        int index =0;
-        
-        for(int i=0; i<s.length(); i++){    
-            char word = s.charAt(i);
+        int idx = 0;
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
             
-            if(word == ' '){
+            if(c == ' '){
                 sb.append(' ');
-                index =0;
-            }else {
-                if(index%2 == 0){
-                    sb.append(Character.toUpperCase(word));
-                }else {
-                    sb.append(Character.toLowerCase(word));
+                idx =0;
+            }else{
+                if(idx%2 == 0){
+                    sb.append(Character.toUpperCase(c));
+                }else{
+                    sb.append(Character.toLowerCase(c));
                 }
                 
-                index++;
+                idx++;
             }
-
+            
         }
-
+        
         return sb.toString();
     }
 }
