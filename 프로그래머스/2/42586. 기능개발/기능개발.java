295301f7a77bddc987c524c1fd2360926446a5b3
Nana <102345450+sksmsdlskgus@users.noreply.github.com>
2025-12-23 17:22:06 +0900

    [level 2] Title: 기능개발, Time: 3.93 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,47 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 뒤에있는 기능이 앞에있는 기능보다 먼저 개발될 수 있음
-        // 뒤에있는 기능은 앞에있는 기능이 배포될때 함께 배포됨 
-        // 순서대로 -> 선입선출
-        // progresses = 작업의 진도
-        // speeds = 각 작업의 개발 속도가 적힌 정수배열
-        // 각 배포마다 몇개의 기능이 배포 되는지 ? 최종 return
-        // 배포는 하루에 한 번 ,하루의 끝에 이루어짐
-        // 예를 들어 진도율이 95%인 작업의 개발 속도가 -> 하루에 4%라면 -> 배포는 2일 뒤
-        // 100-진도율 / 속도 = 남은 일수 -> 반올림 -> 큐에 담아 
-        // 큐에 담고 배포 수 리스트 생성 -> 배포수 카운트 적립
-        // 큐가 비지 않았으면-> 앞에 작업부터 스캔 -> 비지않고 앞에꺼가 일수가 작거나 같으면 ++
-        // 다돌면 리스트에 담기 -> 최종 배열에 담아 리턴
-  
-        Queue<Integer> days = new LinkedList<>();
-
-        for (int i = 0; i < speeds.length; i++) {
-            int day = (int) Math.ceil((100-progresses[i])/ (double)speeds[i]) ;  // 아 더블       
-            days.offer(day);
-            
-          }
+        List<Integer> list = new LinkedList<>();
         
-         List<Integer> result = new ArrayList<>();
-        while(!days.isEmpty()){
-            int current = days.poll(); // 가장 앞의 작업
-            int count = 1;
+        int length = speeds.length;
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0; i<length; i++){
+            int x = (100 - progresses[i])/speeds[i]; // 일수
             
-            while (!days.isEmpty() && days.peek() <= current) {
-                count++;
-                days.poll();
+            if((100 - progresses[i])%speeds[i] != 0){
+                x++;
             }
-             result.add(count);
+            
+            q.offer(x);
         }
-       
         
-      int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
+        
+        while (!q.isEmpty()) {
+            int day = q.poll();
+            int count = 1;
 
-   
+            while (!q.isEmpty() && q.peek() <= day) {
+                q.poll();
+                count++;
+            }
+
+            list.add(count);
+        }
         
-        return answer;
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
